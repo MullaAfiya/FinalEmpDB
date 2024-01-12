c1bfdade6041389c3fc1dd6bba59b33581c904c9
MullaAfiya <mullaafiya@gmail.com>
2024-01-12 21:55:31 +0530

    first commit

---
+++ dbo/Tables/Department.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [DepartmentId]   INT            IDENTITY (1, 1) NOT NULL,
+    [DepartmentName] NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([DepartmentId] ASC)
+);
+
---
+++ dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]    INT           IDENTITY (1, 1) NOT NULL,
+    [EmployeeName]  VARCHAR (100) NULL,
+    [Department]    VARCHAR (50)  NULL,
+    [DateOfJoining] DATE          NULL,
+    [PhotoFileName] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
